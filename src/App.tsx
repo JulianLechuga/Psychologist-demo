1dba30942bf4c2fab89c5ee2e1d6ab72adffc854
Julian Lechuga <julianlechuga12@gmail.com>
2026-03-12 01:07:28 +0100

    Fix router basename and build script

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ const ScrollToTop = () => {
 
 function App() {
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <ScrollToTop />
       <Routes>
         <Route path="/" element={<Layout />}>
